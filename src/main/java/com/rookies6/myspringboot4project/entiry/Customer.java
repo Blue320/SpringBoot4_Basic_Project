2c85e822b8fa0ee1de2c1d1353f9c8163f735b2d
Blue320 <susan032052@gmail.com>
2026-09-01 15:51:06 +0900

    LocalDateTime add

--- src/main/java/com/rookies6/myspringboot4project/entiry/Customer.java
+++ src/main/java/com/rookies6/myspringboot4project/entiry/Customer.java
@@ -1,12 +1,23 @@
 package com.rookies6.myspringboot4project.entiry;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "customers")
 public class Customer {
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String customerId;
 
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createAt = LocalDateTime.now();
 }
